e6f4e33658b7c5e74a8d1d5d7771f4810ab8fb45
Natali <n_sunberry@inbox.ru>
2022-08-21 19:59:14 +0300

    Задачи

--- Practice9/Program.cs
+++ Practice9/Program.cs
@@ -1,20 +1,24 @@
 ﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
 double[] array = new double[10];
-double min = 0;
-double max = 0;
-for (int i = 0; i < array.Length; i++)
+
+for(int i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().NextDouble(1, 20);
+    array[i] = new Random().NextDouble();
+}
+double min = array[0];
+double max = array[0];
+for(int i = 0; i < array.Length; i++)
+{
     if (array[i] < min)
-    {
-        min = array[i];
-    }
+    
+    min = array[i];
+
     if (array[i] > max)
     {
         max = array[i];
     }
     Console.Write(array[i] + " ");
-}
+}   
 Console.WriteLine();
 Console.WriteLine(max - min);
 
